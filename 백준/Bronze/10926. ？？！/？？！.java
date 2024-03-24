b02d9afde8984c860a0ea8da58382a50866c8eac
geg222 <147246023+geg222@users.noreply.github.com>
2024-03-24 19:20:43 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,13 +1,9 @@
-
-
 import java.util.Scanner;
 
- class Main {
-     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         String a = sc.next();
-
-         System.out.println(a+"??!");
-     }
- }
-
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        System.out.println(A+"??!");
+        }
+    }
