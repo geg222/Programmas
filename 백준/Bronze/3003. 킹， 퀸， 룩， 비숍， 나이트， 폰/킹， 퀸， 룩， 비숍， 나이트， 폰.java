a47e834e89d36562273ec3b14a345c0fd95f1c62
geg222 <147246023+geg222@users.noreply.github.com>
2024-03-25 09:19:18 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 232 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        int A = 1-a;
+        int B = 1-b;
+        int C = 2-c;
+        int D = 2-d;
+        int E = 2-e;
+        int F = 8-f;
+
+        System.out.print(A+" "); System.out.print(B+" "); System.out.print(C+" ");
+        System.out.print(D+" "); System.out.print(E+" "); System.out.print(F+" ");
+
+
+        }
+    }
