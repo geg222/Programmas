2e0d30e9572d7387a182390ceab4383976517f6a
geg222 <147246023+geg222@users.noreply.github.com>
2024-03-28 09:05:04 +0900

    [Bronze V] Title: 카드 게임, Time: 208 ms, Memory: 17648 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum =0;
+        for(int i=0; i<5; i++) {
+            int a = sc.nextInt();
+            sum +=a;
+        }
+        System.out.println(sum);
+
+
+
+        }
+    }
