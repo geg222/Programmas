b979e2249118b0eb062012858fc0d17e45cc326c
geg222 <147246023+geg222@users.noreply.github.com>
2024-04-15 09:24:06 +0900

    [Bronze IV] Title: 과자, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int K = sc.nextInt();
+       int N = sc.nextInt();
+       int M = sc.nextInt();
+       if(M>=K*N ){
+           System.out.println("0");
+       }
+       else System.out.println(K*N-M);
+    }
+    }
+
+
+
