bb064f036817b362d7981133e50bd6fd4e2faf43
geg222 <147246023+geg222@users.noreply.github.com>
2024-05-11 17:43:04 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N =sc.nextInt();
+        int [] A = new int [N];
+        for(int i=0; i<N; i++){
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        System.out.println(A[0] * A[N-1]);
+    }
+}
+
+
+
+
+
+
