a325f3ae06e78ec9efd64f353e1155c8ec73b0a2
geg222 <147246023+geg222@users.noreply.github.com>
2024-05-20 09:47:51 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 212 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String A =sc.nextLine();
+        if(A.equals("NLCS")) System.out.print("North London Collegiate School");
+        else if(A.equals("BHA")) System.out.print("Branksome Hall Asia");
+        else if(A.equals("KIS")) System.out.print("Korea International School");
+        else if(A.equals("SJA")) System.out.print("St. Johnsbury Academy");
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
