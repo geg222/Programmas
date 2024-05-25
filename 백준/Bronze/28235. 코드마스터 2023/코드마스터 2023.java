326605454a4b7f2d883804f85199d5e6c71880b8
geg222 <147246023+geg222@users.noreply.github.com>
2024-05-25 21:37:04 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 196 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+            String A =sc.nextLine();
+            if(A.equals("SONGDO")) System.out.println("HIGHSCHOOL");
+            else if(A.equals("CODE")) System.out.println("MASTER");
+            else if(A.equals("2023")) System.out.println("0611");
+            else System.out.println("CONTEST");
+        }
+    }
+
+
+
+
+
+
+
+
