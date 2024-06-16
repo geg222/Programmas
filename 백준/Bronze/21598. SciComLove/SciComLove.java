7f66415764cd3925c344c7a05d9ee63f96f447f1
geg222 <147246023+geg222@users.noreply.github.com>
2024-06-16 20:31:24 +0900

    [Bronze V] Title: SciComLove, Time: 204 ms, Memory: 17564 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int N =sc.nextInt();
+        for(int i=0; i<N; i++){
+            System.out.println("SciComLove");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
